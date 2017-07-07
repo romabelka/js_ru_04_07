32b17656a8168077d2a80c91d4999aaff59877b3
Tor <Brisstor@yahoo.com/>
2017-07-07 21:54:15 +0300

    ht-1 Added comments list

--- src/Article.js
+++ src/Article.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react'
 
+import CommentsList from './CommentsList'
+
 class Article extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             isOpen: false
@@ -10,39 +12,28 @@ class Article extends Component {
     }
 
     render() {
-        const { article } = this.props
+        const { article } = this.props;
         return (
             <div>
                 <h3 onClick = {this.handleClick}>{article.title}</h3>
                 {this.getBody()}
+                {article.comments ? <CommentsList comments = {article.comments} /> : null}
             </div>
         )
     }
 
     getBody() {
-        if (!this.state.isOpen) return null
+        if (!this.state.isOpen) return null;
 
         return <p>{this.props.article.text}</p>
     }
 
     handleClick = (ev) => {
-        ev.preventDefault()
+        ev.preventDefault();
         this.setState({
             isOpen: !this.state.isOpen
         })
     }
 }
-
-/*
-function Article(props) {
-    const { article } = props
-    return (
-        <div>
-            <h3>{article.title}</h3>
-            <p>{article.text}</p>
-        </div>
-    )
-}
-*/
 
 export default Article
---
+++ src/CommentsList.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import Comment from './Comment'
+
+class CommentsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    getComments() {
+        return this.props.comments.map( ({id, user, text}) => <Comment key={id} user={user} text={text}/> )
+    }
+
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>{this.state.isOpen ? 'Hide comments' : 'Show comments'}</button>
+                {this.state.isOpen ? <ul> {this.getComments()} </ul> : null}
+            </div>
+        )
+    }
+}
+
+export default CommentsList

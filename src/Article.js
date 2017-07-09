3d052f4968b5e72f049746b608cc3cae95b4a0bf
AntonDolgov <dlgvnt@gmail.com>
2017-07-10 02:03:58 +0300

    Добавил блоки с комментариями.

--- src/Article.js
+++ src/Article.js
@@ -8,7 +8,7 @@ class Article extends Component {
             isOpen: false
         }
     }
-
+    /*
     render() {
         const { article } = this.props
         return (
@@ -22,11 +22,64 @@ class Article extends Component {
     getBody() {
         if (!this.state.isOpen) return null
 
-        return <p>{this.props.article.text}</p>
+        return (
+              <div>
+                <p>{this.props.article.text}</p>
+                <button>Show comments</button>
+              </div>
+            )
+    }
+    */
+
+    render() {
+        const { article } = this.props
+        return (
+            <div>
+                <h3 onClick = {this.handleClick}>{article.title}</h3>
+                {this.showArticle()}
+            </div>
+        )
+    }
+
+    getBody(body) {
+        if (!this.state.isOpen) return null;
+
+        return body;
+    }
+
+    showArticle() {
+        const body =  <div>
+                        <p>{this.props.article.text}</p>
+                        <button onClick = {this.handleClick}>Show comments</button>
+                        {this.showComments()}
+                      </div>
+
+        return this.getBody(body);
+    }
+
+    showComments() {
+        const comments = this.props.article.comments;
+        let body;
+
+        if(comments !== undefined) {
+
+            body = comments.map(comment =>
+                   <div className="comment" key={comment.id}>
+                      <h4 className="comment__user">{comment.user}</h4>
+                      <p className="comment__text">{comment.text}</p>
+                   </div>
+            );
+
+        } else {
+            body = <div>Comments field is empty</div>;
+        }
+
+        return this.getBody(body);
     }
 
     handleClick = (ev) => {
         ev.preventDefault()
+
         this.setState({
             isOpen: !this.state.isOpen
         })

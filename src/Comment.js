e9511040f933c3ac2d8278c3a072add357c425d1
Kirill Katyshev <kat@bbq.agency>
2017-07-10 11:26:51 +0300

    homework1 by kkatyshev

---
+++ src/Comment.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Comment({ comment }) {
+
+    return (
+        <div>
+            <h5>{comment.user}</h5>
+            <p>{comment.text}</p>
+        </div>
+    )
+}
+
+export default Comment;

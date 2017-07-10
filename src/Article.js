8d01a60bab766ceb62617697e3a189903b74872f
lera <kalinina.valeri@gmail.com>
2017-07-11 01:12:13 +0700

    less1

--- src/Article.js
+++ src/Article.js
@@ -1,48 +1,75 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import CommnetsList from './CommnetsList';
 
 class Article extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            isOpen: false
-        }
+    this.state = {
+      isOpen: false,
+      isComments: false
     }
 
-    render() {
-        const { article } = this.props
-        return (
-            <div>
-                <h3 onClick = {this.handleClick}>{article.title}</h3>
-                {this.getBody()}
-            </div>
-        )
-    }
-
-    getBody() {
-        if (!this.state.isOpen) return null
-
-        return <p>{this.props.article.text}</p>
-    }
-
-    handleClick = (ev) => {
-        ev.preventDefault()
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
-}
-
-/*
-function Article(props) {
-    const { article } = props
+  }
+  render() {
+    const { article } = this.props;
     return (
-        <div>
-            <h3>{article.title}</h3>
-            <p>{article.text}</p>
-        </div>
-    )
-}
-*/
+          <div>
+            <h3 onClick = {this.handleClick}>{ article.title }</h3>
+            { this.getBody() }
 
-export default Article
+          </div>
+    )
+  }
+
+  getBody(){
+
+    if (!this.state.isOpen) return null
+
+    return (
+      <div>
+        <p>{ this.props.article.text }</p>
+        <a href="#" onClick = {this.checkedStateComments}>{ this.getTextBtn() }</a>
+        { this.getCommentsList() }
+      </div>
+    )
+  }
+
+  getCommentsList(){
+    if (!this.state.isComments) return false
+
+    return <CommnetsList comments={this.props.article.comments} />
+  }
+
+  getTextBtn(){
+    if (!this.state.isComments) return 'Open'
+
+    return 'Close'
+  }
+
+  handleClick = (ev) => {
+    ev.preventDefault();
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  checkedStateComments = (ev) => {
+    ev.preventDefault();
+    this.setState({
+      isComments: !this.state.isComments
+    })
+  }
+}
+
+// function Article(props) {
+//   const { article } = props;
+//   return (
+//     <div>
+//       <h3>{ article.title }</h3>
+//       <p>{ article.text }</p>
+//     </div>
+//   )
+// }
+
+export default Article;

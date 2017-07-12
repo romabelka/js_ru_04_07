6f56b873a3a40bb5409aaac2a5927684c6a5643e
Vitaly Yerofeyevsky <flytaly@NiceMac.local>
2017-07-12 21:14:28 +0300

    change ArticleList from Class Component to Functional Components

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -3,25 +3,23 @@ import Article from './Article'
 import PropTypes from 'prop-types'
 import toggleOpenArticle from '../decorators/accordeon'
 
-class ArticlesList extends Component {
+function ArticlesList(props) {
+    const {articles, state, toggleOpenArticle} = props
+    const articleElements = articles.map(article => (
+        <li key={article.id}>
+            <Article
+                article={article}
+                isOpen={article.id === state.openArticleId}
+                toggleOpen={toggleOpenArticle(article.id)}
+            />
+        </li>
+    ))
 
-    render() {
-        const {articles, state, toggleOpenArticle} = this.props
-        const articleElements = articles.map(article => (
-            <li key={article.id}>
-                <Article
-                    article={article}
-                    isOpen={article.id === state.openArticleId}
-                    toggleOpen={toggleOpenArticle(article.id)}
-                />
-            </li>
-        ))
-        return (
-            <ul>
-                {articleElements}
-            </ul>
-        )
-    }
+    return (
+        <ul>
+            {articleElements}
+        </ul>
+    )
 }
 
 ArticlesList.propTypes = {

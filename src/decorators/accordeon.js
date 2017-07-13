ee1d9016f7414aee5d54e5acfb070ac4703c6f12
seryozhatebe <pbiska@list.ru>
2017-07-13 18:16:09 +0300

    HT2

---
+++ src/decorators/accordeon.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+
+export default (OriginalComponet) => class DecoratedComponent extends React.Component {
+    state = {
+        openArticleId: null
+    };
+
+    toggleOpenArticle = openArticleId => () => {
+        if (this.state.openArticleId === openArticleId) {
+            this.setState({
+                openArticleId: null
+            })
+        } else {
+            this.setState({openArticleId})
+        }
+    };
+
+    render() {
+        return <OriginalComponet {...this.props} state={this.state.openArticleId} toggleOpenArticle={this.toggleOpenArticle}/>
+    }
+
+}

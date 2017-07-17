dfcd0934e0865800a6562d03b2928c0f33e06157
Vitaliy Koshovyi <bandicoot39@gmail.com>
2017-07-17 20:17:33 +0300

    ht3 done

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -5,20 +5,57 @@ class CommentForm extends Component {
     super(props);
     this.state = {
       inputVal: '',
+      inputValError: false,
       textVal: '',
+      textValError: false,
     }
   }
-  onChange(ev, stateKey) {
+  changeHandler(ev, stateKey) {
+    const constfieldValue = ev.target.value;
+
+    if (stateKey === 'inputVal') {
+      if (constfieldValue.length >= 30) return
+      this.validateMinLength(constfieldValue, stateKey, 10)
+
+    } else if (stateKey === 'textVal') {
+      if (constfieldValue.length >= 150) return
+      this.validateMinLength(constfieldValue, stateKey, 30)
+    }
+
     this.setState({
       [stateKey]: ev.target.value
     });
   }
+
+  validateMinLength(val, stateKey, minLength) {
+    val.length <= minLength ?
+        this.setState({[`${stateKey}Error`]: true}) :
+        this.setState({[`${stateKey}Error`]: false})
+  }
+
   render() {
+    const errorStyle = {
+      border: '1px solid red'
+    }
+    const style = {
+      border: '1px solid #000'
+    }
     return(
-      <div>
-        <input type="text" value={this.state.inputVal} onChange={(ev) => {this.onChange(ev, 'inputVal')}}/>
-        <textarea value={this.state.textVal} onChange={(ev) => {this.onChange(ev, 'textVal')}}></textarea>
-      </div>
+      <form>
+        <input type="text"
+               value={this.state.inputVal}
+               onChange={(ev) => {this.changeHandler(ev, 'inputVal')}}
+               style={this.state.inputValError ? errorStyle : style}
+        />
+        <br/>
+        <br/>
+        <textarea value={this.state.textVal}
+                  onChange={(ev) => {this.changeHandler(ev, 'textVal')}}
+                  style={this.state.textValError ? errorStyle : style}>
+        </textarea>
+        <br/>
+        <button>Send</button>
+      </form>
     );
   }
 }

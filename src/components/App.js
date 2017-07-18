0212e83df5cc6a39b03bef68acf08814bd5b669a
Kirill Katyshev <kat@bbq.agency>
2017-07-18 09:57:46 +0300

    HM#3 by K.Katyshev

--- src/components/App.js
+++ src/components/App.js
@@ -2,36 +2,111 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
-import UserForm from './UserForm'
-import Select from 'react-select'
-import 'react-select/dist/react-select.css'
+import moment from 'moment'
+import DayPicker, { DateUtils } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
+
+const dayPickerInitialState = {
+    from: null,
+    to: null,
+    enteredTo: null, // Keep track of the last day for mouseEnter.
+};
 
 class App extends Component {
     static propTypes = {
-
+        articles: PropTypes.array
     };
 
     state = {
-        selected: null
+        selected: null,
+        ...dayPickerInitialState
     }
 
     render() {
         const {articles} = this.props
-        const options = articles.map(article => ({
-            label: article.title,
-            value: article.id
-        }))
+        const { from, to, enteredTo } = this.state;
+        const modifiers = { start: from, end: enteredTo };
+        const disabledDays = { before: this.state.from };
+        const selectedDays = [from, { from, to: enteredTo }];
         return (
             <div>
-                <UserForm />
-                <Select options = {options} onChange = {this.handleSelect} value = {this.state.selected} multi />
+                {!from && !to && <p>Please select the <strong>first day</strong>.</p>}
+                {from && !to && <p>Please select the <strong>last day</strong>.</p>}
+                {from &&
+                to &&
+                <p>
+                    You chose from
+                    {' '}
+                    {moment(from).format('L')}
+                    {' '}
+                    to
+                    {' '}
+                    {moment(enteredTo).format('L')}
+                    .
+                    {' '}
+                    <a onClick={this.dayPickerReset}>Reset</a>
+                </p>}
+                <DayPicker
+                    className="Range"
+                    numberOfMonths={2}
+                    fromMonth={from}
+                    fixedWeeks
+                    selectedDays={selectedDays}
+                    disabledDays={disabledDays}
+                    modifiers={modifiers}
+                    onDayClick={this.handleDayClick}
+                    onDayMouseEnter={this.handleDayMouseEnter}
+                />
                 <ArticleList articles = {articles}/>
                 <ArticlesChart articles = {articles}/>
             </div>
         )
     }
 
-    handleSelect = selected => this.setState({ selected })
+    isSelectingFirstDay(from, to, day) {
+        const firstDayIsNotSelected = !from;
+        const selectedDayIsBeforeFirstDay = from && DateUtils.isDayBefore(day, from);
+        const rangeIsSelected = from && to;
+        return (
+            firstDayIsNotSelected || selectedDayIsBeforeFirstDay || rangeIsSelected
+        );
+    }
+
+    handleDayClick = day => {
+        const { from, to } = this.state;
+
+        if (from && to && day >= from && day <= to) {
+            this.reset();
+            return;
+        }
+
+        if (this.isSelectingFirstDay(from, to, day)) {
+            this.setState({
+                from: day,
+                to: null,
+                enteredTo: null,
+            });
+        } else {
+            this.setState({
+                to: day,
+                enteredTo: day,
+            });
+        }
+    };
+
+    handleDayMouseEnter = day => {
+        const { from, to } = this.state;
+
+        if (!this.isSelectingFirstDay(from, to, day)) {
+            this.setState({
+                enteredTo: day,
+            });
+        }
+    };
+
+    dayPickerReset = () => {
+        this.setState(dayPickerInitialState);
+    };
 }
 
 export default App

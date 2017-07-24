1b18fc18b3e4a2e941964991f0e8288d5cc500ce
Timur Ataev <t.ataev@lucky-labs.com>
2017-07-24 19:47:18 +0300

    Add some code

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import DateRange from './DateRange'
 import SelectFilter from './Select'
+import { connect } from 'react-redux';
 
 class Filters extends Component {
     static propTypes = {
+        articles: PropTypes.array.isRequired,
     };
 
     render() {
         return (
             <div>
-                <SelectFilter articles = {[]} />
+                <SelectFilter articles = { this.props.articles } />
                 <DateRange />
             </div>
         )
     }
 }
 
-export default Filters
+export default connect(({articles}) => ({articles}))(Filters)
